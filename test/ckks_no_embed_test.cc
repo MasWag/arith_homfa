092133c0badd064dea95a0c7fd51209bcf025407
Masaki Waga <masakiwaga@gmail.com>
2023-06-19 18:57:34 +0900

    Added test cases of CKKS Encoder with encryption/decryption

--- test/ckks_no_embed_test.cc
+++ test/ckks_no_embed_test.cc
@@ -5,26 +5,94 @@
 
 BOOST_AUTO_TEST_SUITE(CKKSNoEmbedTest)
 
-    RC_BOOST_PROP(encodeAndDecode,
-                  (const bool& isNegative, const uint64_t& intValue)) {
-        // We implicitly require that the given value is not too large. Otherwise the encoding fails.
-        const double scale = std::pow(2.0, 45);
-        const double &value = isNegative ? -static_cast<double>(intValue) / scale : static_cast<double>(intValue) / scale;
+  struct CKKSNoEmbedTestFixture {
+    const double scale = std::pow(2.0, 40);
+    const double minValue = 0.001;
+    seal::EncryptionParameters parms;
+    const std::size_t poly_modulus_degree = 8192;
+    std::vector<seal::Modulus> smallModulus;
+    std::vector<seal::Modulus> largeModulus;
+    seal::Plaintext plain;
+    CKKSNoEmbedTestFixture() : parms(seal::scheme_type::ckks) {
+      parms.set_poly_modulus_degree(poly_modulus_degree);
+      smallModulus = seal::CoeffModulus::Create(poly_modulus_degree, {60, 40, 60});
+      largeModulus = seal::CoeffModulus::Create(poly_modulus_degree, {60, 40, 40, 60});
+    };
+  };
 
-        seal::EncryptionParameters parms(seal::scheme_type::ckks);
-        const std::size_t poly_modulus_degree = 8192;
-        parms.set_poly_modulus_degree(poly_modulus_degree);
-        parms.set_coeff_modulus(seal::CoeffModulus::Create(poly_modulus_degree, { 60, 40, 60 }));
-        seal::SEALContext context(parms);
+  RC_BOOST_FIXTURE_PROP(encodeAndDecode, CKKSNoEmbedTestFixture,
+                        (const int32_t& intValue, const bool& useLargerParam)) {
+    // We implicitly require that the given value is not too large. Otherwise, the encoding fails.
+    const double &value = static_cast<double>(intValue) * minValue;
 
-        // Encode the given value
-        ArithHomFA::CKKSNoEmbedEncoder encoder(context);
-        seal::Plaintext plain;
-        encoder.encode(value, scale, plain);
+    parms.set_coeff_modulus(useLargerParam ? largeModulus : smallModulus);
+    seal::SEALContext context(parms);
 
-        // The result may not be exactly the same but should be close
-        const double epsilon = 0.0001;
-        RC_ASSERT(std::abs(encoder.decode(plain) - value) < epsilon);
-    }
+    // Encode the given value
+    ArithHomFA::CKKSNoEmbedEncoder encoder(context);
+    encoder.encode(value, scale, plain);
+
+    // The result may not be exactly the same but should be close
+    const double epsilon = 0.0001;
+    RC_ASSERT(std::abs(encoder.decode(plain) - value) < epsilon);
+  }
+
+  RC_BOOST_FIXTURE_PROP(encodeEncryptDecryptAndDecode, CKKSNoEmbedTestFixture,
+                        (const int32_t& intValue, const bool& useLargerParam)) {
+    // We implicitly require that the given value is not too large. Otherwise, the encoding fails.
+    const double &value = static_cast<double>(intValue) * minValue;
+
+    parms.set_coeff_modulus(useLargerParam ? largeModulus : smallModulus);
+    seal::SEALContext context(parms);
+    // Generate Key
+    seal::KeyGenerator keygen(context);
+    const auto &secretKey = keygen.secret_key();
+
+    // Encode the given value
+    ArithHomFA::CKKSNoEmbedEncoder encoder(context);
+    seal::Encryptor encryptor(context, secretKey);
+    seal::Decryptor decryptor(context, secretKey);
+    seal::Ciphertext cipher;
+    encoder.encode(value, scale, plain);
+    encryptor.encrypt_symmetric(plain, cipher);
+    decryptor.decrypt(cipher, plain);
+
+    // The result may not be exactly the same but should be close
+    const double epsilon = 0.0001;
+    RC_ASSERT(std::abs(encoder.decode(plain) - value) < epsilon);
+  }
+
+  RC_BOOST_FIXTURE_PROP(encodeEncryptDecryptAndDecodeAfterRescale, CKKSNoEmbedTestFixture,
+                        (const int16_t& intValue, const bool& useLargerParam)) {
+    // We implicitly require that the given value is not too large. Otherwise, the encoding fails.
+    const double &value = static_cast<double>(intValue) * minValue;
+
+    parms.set_coeff_modulus(useLargerParam ? largeModulus : smallModulus);
+    seal::SEALContext context(parms);
+    // Generate Key
+    seal::KeyGenerator keygen(context);
+    const auto &secretKey = keygen.secret_key();
+    seal::RelinKeys relinKeys;
+    keygen.create_relin_keys(relinKeys);
+
+    // Encode the given value
+    ArithHomFA::CKKSNoEmbedEncoder encoder(context);
+    seal::Encryptor encryptor(context, secretKey);
+    seal::Decryptor decryptor(context, secretKey);
+    seal::Ciphertext cipher, absCipher;
+    encoder.encode(value, scale, plain);
+    encryptor.encrypt_symmetric(plain, cipher);
+    encoder.encode(std::abs(value), scale, plain);
+    encryptor.encrypt_symmetric(plain, absCipher);
+    seal::Evaluator evaluator(context);
+    evaluator.multiply_inplace(cipher, absCipher);
+    evaluator.relinearize_inplace(cipher, relinKeys);
+    evaluator.rescale_to_next_inplace(cipher);
+    decryptor.decrypt(cipher, plain);
+
+    // The result may not be exactly the same but should be close
+    const double epsilon = 0.0001;
+    RC_ASSERT(std::abs(encoder.decode(plain) - value * std::abs(value)) < epsilon);
+  }
 
 BOOST_AUTO_TEST_SUITE_END()
