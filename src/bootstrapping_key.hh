683135be6fc6af99509706e15a1a0f782e88cdb4
Masaki Waga <masakiwaga@gmail.com>
2023-08-20 10:07:54 +0900

    Use std::make_shared

--- src/bootstrapping_key.hh
+++ src/bootstrapping_key.hh
@@ -31,11 +31,9 @@ namespace ArithHomFA {
 
     BootstrappingKey(const TFHEpp::SecretKey &skey, const TFHEpp::Key<TFHEpp::lvl3param> &lvl3key,
                      const TFHEpp::Key<typename mid2lowP::targetP> &lvlhalfkey)
-        : BKey(skey) {
-      kskh2m = std::make_unique<TFHEpp::KeySwitchingKey<high2midP>>();
-      kskm2l = std::make_unique<TFHEpp::KeySwitchingKey<mid2lowP>>();
-      bkfft = std::make_unique<TFHEpp::BootstrappingKeyFFT<brP>>();
-
+        : BKey(skey), kskh2m(std::make_shared<TFHEpp::KeySwitchingKey<high2midP>>()),
+          kskm2l(std::make_shared<TFHEpp::KeySwitchingKey<mid2lowP>>()),
+          bkfft(std::make_shared<TFHEpp::BootstrappingKeyFFT<brP>>()) {
       TFHEpp::ikskgen<high2midP>(*kskh2m, lvl3key, skey.key.get<typename high2midP::targetP>());
       TFHEpp::ikskgen<mid2lowP>(*kskm2l, skey.key.get<typename mid2lowP::domainP>(), lvlhalfkey);
       TFHEpp::bkfftgen<brP>(*bkfft, lvlhalfkey, skey.key.get<typename brP::targetP>());
